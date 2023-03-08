2c42fe1062d375cf9abbe5b4a3f07ca17974ecb2
이인수 <in63119@gmail.com>
2023-03-08 22:53:04 +0900

    test Hello World

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello, I'm InBrew!</Text>
+    </View>
+  );
+}
+
+export default App;
