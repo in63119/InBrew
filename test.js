2991e06f0e3c9741ec9b5d16ca73129cd1703f0f
이인수 <in63119@gmail.com>
2023-04-21 23:25:52 +0900

    조합

--- test.js
+++ test.js
@@ -1,26 +1,37 @@
 //순열 구하기
-const permutation = (arr, n) => {
-  const result = [];
+const getPermutation = function (arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map(value => [value]);
 
-  // DFS
-  const dfs = (i, arr) => {
-    // 탈출
-    if (i === arr.length) {
-      return result.push([...arr.slice(0, n)]);
-    }
+  arr.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutations = getPermutation(rest, selectNumber - 1);
+    const attached = permutations.map(permutation => [fixed, ...permutation]);
+    results.push(...attached);
+  });
 
-    for (let j = i; j < arr.length; j++) {
-      //swap
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-      //dfs
-      dfs(i + 1, arr);
-      //swap back
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-  };
-
-  dfs(0, arr);
-  return result;
+  return results;
 };
 
-console.log(permutation(['a', 'b', 'c', 'd'], 3));
+const example = [1, 2, 3, 4];
+const result = getPermutation(example, 3);
+//   console.log(result);
+
+// 조합 구하기
+const getCombinations = function (arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map(value => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
+    const combinations = getCombinations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
+    const attached = combinations.map(combination => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+    results.push(...attached); // 배열 spread syntax 로 모두다 push
+  });
+
+  return results; // 결과 담긴 results return
+};
+
+const example = [1, 2, 3, 4];
+const result = getCombinations(example, 3);
+console.log(result);
